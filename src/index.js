99f6ed12d39746e57c7e3311922242be6a33ca60
Richard Afro <richyafro@hotmail.com>
2020-03-13 11:31:49 +0100

    move routes to home page

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Auth from './pages/auth';
-import AppProvider from './providers';
+import App from './App';
 
-const Routes = () => {
-  return (
-    <AppProvider>
-      <Router>
-        <Switch>
-          <Route path='/auth' component={Auth} />
-          <Route path='/' exact component={App} />
-        </Switch>
-      </Router>
-    </AppProvider>
-  );
-};
-
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
