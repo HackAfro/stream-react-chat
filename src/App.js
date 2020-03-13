9a0a78e67d57475898e649f5d231e98a540a7439
Richard Afro <richyafro@hotmail.com>
2020-03-13 11:31:30 +0100

    move routes to home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Auth from './pages/auth';
 import Home from './pages/home';
+import AppProvider from './providers';
 
 import './App.css';
 
 const App = () => {
-  return <Home />;
+  return (
+    <AppProvider>
+      <Router>
+        <Switch>
+          <Route path='/auth' component={Auth} />
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
+    </AppProvider>
+  );
 };
 
 export default App;
